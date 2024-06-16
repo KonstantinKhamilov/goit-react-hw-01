d3cb242bec34c53cd6aefb19caf7fbbf6072a3e6
KonstantinKhamilov <134136455+KonstantinKhamilov@users.noreply.github.com>
2024-06-16 20:53:46 +0300

    ошибки в консоли на файл профайл.jsx. Сборка без ошибок.

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,28 @@
+import style from "./Profile.module.css";
+console.log(style);
+export default function Profile({ name, tag, location, image, stats }) {
+  return (
+    <div className={style.profile}>
+      <div>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+      <ul className={style.ul}>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./components/App/App.jsx";
+import Profile from "./components/Profile/Profile.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Profile />
   </React.StrictMode>
 );
