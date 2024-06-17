d23b87a692f4892e5bdfa615b7006ff4fe48059a
KonstantinKhamilov <134136455+KonstantinKhamilov@users.noreply.github.com>
2024-06-17 23:38:00 +0300

    первая часть выполненна

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,24 +3,24 @@ console.log(style);
 export default function Profile({ name, tag, location, image, stats }) {
   return (
     <div className={style.profile}>
-      <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+      <div className={style.firstDiv}>
+        <img className={style.image} src={image} alt="User avatar" />
+        <p className={style.Name}>{name}</p>
+        <p className={style.text}>@{tag}</p>
+        <p className={style.text}>{location}</p>
       </div>
-      <ul className={style.ul}>
+      <ul>
         <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+          <span className={style.liSpan}>Followers</span>
+          <span className={style.spanStats}>{stats.followers}</span>
         </li>
         <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
+          <span className={style.liSpan}>Views</span>
+          <span className={style.spanStats}>{stats.views}</span>
         </li>
         <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+          <span className={style.liSpan}>Likes</span>
+          <span className={style.spanStats}>{stats.likes}</span>
         </li>
       </ul>
     </div>
